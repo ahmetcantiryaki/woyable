bea78b3a71523b5701d4447ee0528d92cc840eb9
ahmetcantryk <ahmetcan.1855@gmail.com>
2026-02-03 04:59:22 +0300

    feat: Kök düzeni oluşturuldu ve Cookiebot ile Google Analytics entegre edildi.

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ export const metadata = {
     description: 'Woyable Dijital Ajans',
 };
 
+import Script from 'next/script';
+
 export default function RootLayout({
     children,
 }: {
@@ -17,7 +19,31 @@ export default function RootLayout({
 }) {
     return (
         <html lang="tr">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                {/* Cookiebot */}
+                <Script
+                    id="Cookiebot"
+                    src="https://consent.cookiebot.com/uc.js"
+                    data-cbid="6bc6a691-10a9-4861-8499-a5c6c451c31e"
+                    strategy="afterInteractive"
+                />
+
+                {/* Google Analytics */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-6JT4FYTX59"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-6JT4FYTX59');
+                    `}
+                </Script>
+
+                {children}
+            </body>
         </html>
     );
 }
